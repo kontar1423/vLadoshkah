f78a3cf6c1ed4a8afb8e0c45a0c2e93b765430aa
Konstantin Tarasenko <kostatar2007@gmail.com>
2025-10-27 01:20:22 +0300

    fix: migration

--- migrations/1758042825919_create_shelters.cjs
+++ migrations/1758042825919_create_shelters.cjs
@@ -1,3 +1,13 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.up = (pgm) => {
   pgm.createTable('shelters', {
     id: 'id',
@@ -25,7 +35,11 @@ exports.up = (pgm) => {
     }
   });
 };
-
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.down = (pgm) => {
   pgm.dropTable('shelters');
 };
--- migrations/1758138265919_create_animals.cjs
+++ migrations/1758138265919_create_animals.cjs
@@ -1,4 +1,14 @@
-/* eslint-disable camelcase */
+  /* eslint-disable camelcase */
+  /**
+   * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+   */
+  exports.shorthands = undefined;
+
+  /**
+   * @param pgm {import('node-pg-migrate').MigrationBuilder}
+   * @param run {() => void | undefined}
+   * @returns {Promise<void> | void}
+   */
 exports.up = (pgm) => {
   pgm.createTable('animals', {
     id: 'id',
@@ -20,7 +30,11 @@ exports.up = (pgm) => {
     history: { type: 'varchar(1000)' },
   });
 };
-
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
 exports.down = (pgm) => {
   pgm.dropTable('animals');
 };
