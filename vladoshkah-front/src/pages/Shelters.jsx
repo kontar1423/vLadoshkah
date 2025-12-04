5b8fce1b9a4833d7808c7a380df4e4cb480ebceb
Konstantin Tarasenko <kostatar2007@gmail.com>
2025-12-04 23:07:39 +0300

    fix: shelters computer view

--- vladoshkah-front/src/pages/Shelters.jsx
+++ vladoshkah-front/src/pages/Shelters.jsx
@@ -309,7 +309,7 @@ const Shelters = () => {
 
           {shelters.length > 0 && (
             <>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-4 md:gap-6 justify-items-center sm:justify-items-start">
+              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-1 gap-2 sm:gap-4 md:gap-8">
                 {currentShelters.map((shelter) => (
                   isMobile ? (
                     <MiniShelterCard 
